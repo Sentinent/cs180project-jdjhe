8cbc904b07a5a71ec7ddcbb6d909ed8f8d6f969a
pjsrcool <justinpham79@outlook.com>
2021-10-12 01:14:12 -0700

    testing calling parser code

--- backend/pycode/test.py
+++ backend/pycode/test.py
@@ -1,9 +1,11 @@
-def run():
-    const data[] = {['first name', 'last name'],['Diane', 'Shan'],['Ethan','Bayer'],['Heng','Tan'],['Jason','Lin'],['Justin','Pham']}
+# def run():
+#     const data[] = {['first name', 'last name'],['Diane', 'Shan'],['Ethan','Bayer'],['Heng','Tan'],['Jason','Lin'],['Justin','Pham']}
+#     print("python code print kk")
+#     return "run function"
 
+# return run()
 
+import "../../data_structures/parsing.py"
 
-    print("python code print kk")
-    return "run function"
-
-return run()
+parser = new Parsing()
+parser.ParseViolations()
