ec1051c395d59abb20e7fb99f201061334a561ad
Ethan Bayer <byeres1871@gmail.com>
2021-10-12 18:02:03 -0700

    Gave search function ability to also read throug hthe search parameters along with desired columns.

--- backend/functions/search.py
+++ backend/functions/search.py
@@ -1,6 +1,6 @@
 
 
-def search(pnum, colNames, searchParam):
+def search(pnum, colNames, searchList):
     """
     Function to search for specific columns and parameters for a given csv dataset
     - psize is the amount of cells within the column 
@@ -9,6 +9,7 @@ def search(pnum, colNames, searchParam):
     - searchParam is the list of search parameters for a column
     """
     psize = 10
+    searchParam = searchList
 
     # Determine the file based on the given page number
     if (pnum == 1):
@@ -63,7 +64,43 @@ def search(pnum, colNames, searchParam):
             result.append(searchColumn[:])
             searchColumn.clear()
             index = 0
-        
-    
+        # If the search list is not empty then assume that each search paramter corresponds to its respective column
+        else:
+            skipL1 = 0
+            searchColumn.append(lineL[col])
+            endSearch = 1
+            end = 0
+            # Then traverse through all the rows in the data set 
+            for line in content:
+                # Skip first row
+                if (skipL1 > 0):
+                    lineList = line.split(',')
+                    # If we have already found the desired value, increment the end counter and add a value to our new found search list
+                    if (endSearch == 0):
+                        end += 1
+                        searchColumn.append(lineList[col])
+                    # If we find the value, begin the end of the loop process
+                    # by beginning end counter to reach the desired page size
+                    # Then if we have already found the value and find it again simply increment end counter by 1 to compensate
+                    if (lineList[col] == searchParam[0]):
+                        searchColumn.append(lineList[col])
+                        if (endSearch == 0):
+                            end += 1
+                        endSearch = 0
+                    # Once the end counter finishes, end the loop
+                    if (end >= psize):
+                        break
+                skipL1 += 1
+            # If we have a found search list that has more than the title, add it to the result list
+            if (len(searchColumn) > 1):
+                result.append(searchColumn[:])
+            searchColumn.clear()
+            index = 0
+            searchParam.pop(0)
+
+    # If nothing was able to be put in the result list return 0 to indicate failure
+    if (len(result) == 0):
+        return 0
+
     return result
                 
