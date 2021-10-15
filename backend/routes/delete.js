16e08df66de3ca41718727a2a21063f8a904d910
pjsrcool <justinpham79@outlook.com>
2021-10-14 20:49:54 -0700

    ran npm run prettier

--- backend/routes/delete.js
+++ backend/routes/delete.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 
 // this route does the deleting
 router.route("/summonsNum=:sumNum").get((req, res) => {
-    const sumNum = req.params.sumNum;
+  const sumNum = req.params.sumNum;
 
-    console.log("want to delete " + sumNum);
-    res.send("you want to delete row with Summons Number " + sumNum);
-  });
-  
+  console.log("want to delete " + sumNum);
+  res.send("you want to delete row with Summons Number " + sumNum);
+});
+
 module.exports = router;
--- backend/routes/update.js
+++ backend/routes/update.js
@@ -2,40 +2,43 @@ const router = require("express").Router();
 
 // this route is used for debugging
 router.route("/").get((req, res) => {
-    console.log("inside delete update route");
-    res.send("what would you like to delete?");
+  console.log("inside delete update route");
+  res.send("what would you like to delete?");
 });
 
 // this route does the updating
-router.route("/summonsNum=:sumNum&" +
-             "plateID=:plateID&" +
-             "regState=:regState&" +
-             "issDate=:issDate&" +
-             "vTime=:vioTime&" +
-             "vCode=:vioCode&" +
-             "vehMake=:vehMake&" +
-             "vehBody=:vehBody&" +
-             "vehYear=:vehYear&" +
-             "street=:street&" +
-             "county=:county").get((req, res) => {
+router
+  .route(
+    "/summonsNum=:sumNum&" +
+      "plateID=:plateID&" +
+      "regState=:regState&" +
+      "issDate=:issDate&" +
+      "vTime=:vioTime&" +
+      "vCode=:vioCode&" +
+      "vehMake=:vehMake&" +
+      "vehBody=:vehBody&" +
+      "vehYear=:vehYear&" +
+      "street=:street&" +
+      "county=:county"
+  )
+  .get((req, res) => {
     console.log("about to do some updating");
-    
+
     const data = {
-        "Summons Number": req.params.sumNum,
-        "Plate ID": req.params.plateID,
-        "Registration State": req.params.regState,
-        "Issue Date": req.params.issDate,
-        "Violation Time" : req.params.vioTime,
-        "Violation Code": req.params.vioCode,
-        "Vehicle Make": req.params.vehMake,
-        "Vehicle Body Type" : req.params.vehBody,
-        "Vehicle Year" : req.params.vehYear,
-        "Street Name" : req.params.street,
-        "Violation County": req.params.county
-    }
-    
-    
-             
-});
+      "Summons Number": req.params.sumNum,
+      "Plate ID": req.params.plateID,
+      "Registration State": req.params.regState,
+      "Issue Date": req.params.issDate,
+      "Violation Time": req.params.vioTime,
+      "Violation Code": req.params.vioCode,
+      "Vehicle Make": req.params.vehMake,
+      "Vehicle Body Type": req.params.vehBody,
+      "Vehicle Year": req.params.vehYear,
+      "Street Name": req.params.street,
+      "Violation County": req.params.county,
+    };
+
+    res.send(data);
+  });
 
 module.exports = router;
