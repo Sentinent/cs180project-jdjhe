2b1d53a0dda3fd1ebeebdbf5520420b254135f9f
pjsrcool <justinpham79@outlook.com>
2021-10-14 20:44:08 -0700

    added routes to delete, insert, and update data

---
+++ backend/routes/update.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+
+// this route is used for debugging
+router.route("/").get((req, res) => {
+    console.log("inside delete update route");
+    res.send("what would you like to delete?");
+});
+
+// this route does the updating
+router.route("/summonsNum=:sumNum&" +
+             "plateID=:plateID&" +
+             "regState=:regState&" +
+             "issDate=:issDate&" +
+             "vTime=:vioTime&" +
+             "vCode=:vioCode&" +
+             "vehMake=:vehMake&" +
+             "vehBody=:vehBody&" +
+             "vehYear=:vehYear&" +
+             "street=:street&" +
+             "county=:county").get((req, res) => {
+    console.log("about to do some updating");
+    
+    const data = {
+        "Summons Number": req.params.sumNum,
+        "Plate ID": req.params.plateID,
+        "Registration State": req.params.regState,
+        "Issue Date": req.params.issDate,
+        "Violation Time" : req.params.vioTime,
+        "Violation Code": req.params.vioCode,
+        "Vehicle Make": req.params.vehMake,
+        "Vehicle Body Type" : req.params.vehBody,
+        "Vehicle Year" : req.params.vehYear,
+        "Street Name" : req.params.street,
+        "Violation County": req.params.county
+    }
+    
+    
+             
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,15 @@ app.use(express.json());
 app.use(cors());
 
 const mainpage = require("./routes/mainpage");
+const insert = require("./routes/insert");
+const del = require("./routes/delete");
+const update = require("./routes/update");
 // const callpy = require("./routes/callpy");
 
 app.use("/", mainpage);
+app.use("/insert", insert);
+app.use("/delete", del);
+app.use("/update", update);
 // app.use("/py", callpy);
 
 app.listen(port, () => {
