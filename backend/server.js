871fc325a9c77a540953b8b392beb6f6c61bda0f
pjsrcool <jpham079@ucr.edu>
2021-10-17 18:36:38 -0700

    enabled loading into js JSON var with errors

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ app.use("/insert", insert);
 app.use("/delete", del);
 app.use("/update", update);
 
-let JSONDATA;
-fs.access("../parsed_data/data.json", fs.constants.F_OK, (err) => {
+let JSONDATA = {};
+fs.access("../parsed_data/data4.json", fs.constants.F_OK, (err) => {
   console.log("checking if file exists");
   if (err) {
     console.log("file does not exist");
@@ -38,21 +38,29 @@ fs.access("../parsed_data/data.json", fs.constants.F_OK, (err) => {
     pyprog.stderr.on("data", (data) => {
       console.log("error: " + data);
     });
-  } else {
-    console.log("file exists, loading into memory...");
-    fs.readFile("../parsed_data/data.json", 'utf8', function (err, data) {
+  }
+
+  console.log("found data json files, loading into memory...");
+
+  const numFiles = 4; // change this to number of data files
+  for (let i = 1; i <= numFiles; ++i) {
+    console.log("reading data" + i + ".json");
+    fs.readFile("../parsed_data/data" + i + ".json", 'utf8', function (err, data) {
       if (err) {
         console.log(err);
       } else {
         const content = String.fromCharCode(...data)
-        JSONDATA = JSON.parse(content);
-        console.log("successfully loaded data");
+        JSONDATA.append(JSON.parse(content));
+        console.log("finished data" + i);
       }
     });
-    
   }
+    
+  
 });
 
 app.listen(port, () => {
   console.log("Server is running on port: " + port);
 });
+
+module.exports = JSONDATA;
