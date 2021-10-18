299090cf50b14bc511f944cd0ba0a982247b4b15
pjsrcool <jpham079@ucr.edu>
2021-10-17 19:58:28 -0700

    made progress on memory issue

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const {spawn} = require("child_process");
+const {spawnSync} = require("child_process");
 
 var app = express();
 const port = 5000;
@@ -20,16 +21,12 @@ app.use("/delete", del);
 app.use("/update", update);
 
 let JSONDATA = {};
-fs.access("../parsed_data/data4.json", fs.constants.F_OK, (err) => {
+fs.access("../parsed_data/data8.json", fs.constants.F_OK, (err) => {
   console.log("checking if file exists");
   if (err) {
     console.log("file does not exist");
 
-    let pyprog = spawn("python3", ["./functions/main.py"]);
-
-    pyprog.stdout.on("data", function (data) {
-      console.log("read stdout data");
-    });
+    let pyprog = spawnSync("python3", ["./functions/main.py"]);
     
     pyprog.on("close", (msg) => {
       console.log("closing py: ${code}");
@@ -42,7 +39,7 @@ fs.access("../parsed_data/data4.json", fs.constants.F_OK, (err) => {
 
   console.log("found data json files, loading into memory...");
 
-  const numFiles = 4; // change this to number of data files
+  const numFiles = 8;
   for (let i = 1; i <= numFiles; ++i) {
     console.log("reading data" + i + ".json");
     fs.readFile("../parsed_data/data" + i + ".json", 'utf8', function (err, data) {
@@ -54,8 +51,7 @@ fs.access("../parsed_data/data4.json", fs.constants.F_OK, (err) => {
         console.log("finished data" + i);
       }
     });
-  }
-    
+  }  
   
 });
 
