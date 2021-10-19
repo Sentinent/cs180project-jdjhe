b17a6bce5226f06b076c3d542047c6e279cc040d
pjsrcool <jpham079@ucr.edu>
2021-10-18 18:38:29 -0700

    search works with multiple search terms now

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -13,17 +13,21 @@ router
     let pageNum = req.params.pageNum;
     let searchTerms = req.params.searchTerms.split(',');
     const resultsPerPage = 20;
+
     let results = JSONDATA.filter(JSONDATA => JSONDATA[columns] == searchTerms[0]);
+    for (let i = 1; i < searchTerms.length; ++i){
+      console.log(i);
+      let temp = JSONDATA.filter(JSONDATA => JSONDATA[columns] == searchTerms[i]);
+      results = results.concat(temp);
+    }
 
     let page = [];
     const startIndex = ((Number)(pageNum) - 1) * resultsPerPage
     for (let i = startIndex; i < startIndex + 20 && i < results.length; ++i) {
-      // console.log(i);
-      // console.log(results[i]);
       page.push(results[i]);
     }
-    // console.log(results);
-    console.log(page);
+
+    // console.log(page);
     console.log("found server: " + columns + " " + pageNum + " " + searchTerms);
 
     res.send(page);
