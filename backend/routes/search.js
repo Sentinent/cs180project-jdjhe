9f79e1ceec9eb5380c1cbf65378b894ae5c4ae50
pjsrcool <jpham079@ucr.edu>
2021-10-18 18:25:32 -0700

    search works

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const { spawn } = require("child_process");
 
 router.route("/").get((req, res) => {
   console.log("found server");
@@ -9,52 +8,25 @@ router.route("/").get((req, res) => {
 router
   .route("/data/cols=:columns&page=:pageNum&terms=:searchTerms")
   .get((req, res) => {
+    let JSONDATA = require('../server.js')
     let columns = req.params.columns;
     let pageNum = req.params.pageNum;
-    let searchTerms = req.params.searchTerms;
+    let searchTerms = req.params.searchTerms.split(',');
+    const resultsPerPage = 20;
+    let results = JSONDATA.filter(JSONDATA => JSONDATA[columns] == searchTerms[0]);
 
-    let Object = [pageNum, columns, searchTerms];
-
-    // call simple search
-    // let pyprog = spawn("python3", [
-    //   "./functions/main.py",
-    //   Number(pageNum),
-    //   columns,
-    //   20,
-    // ]);
-
-    // pyprog.stdout.on("data", function (data) {
-    //   console.log("reading stdout data");
-    //   console.log(data);
-    //   const msg = String.fromCharCode(...data);
-    //   console.log(msg);
-    //   // const msg = String.fromCharCode.apply(null,data);
-    //   const jjjson = JSON.parse(msg);
-    //   console.log("jjjson: ");
-    //   console.log(jjjson);
-    //   res.send(jjjson);
-    //   // success(data);
-    // });
-
-    // pyprog.on("close", (msg) => {
-    //   console.log("closing py: ${code}");
-    // });
-
-    // pyprog.stderr.on("data", (data) => {
-    //   console.log("error: " + data);
-    //   // nosuccess(data);
-    // });
-
+    let page = [];
+    const startIndex = ((Number)(pageNum) - 1) * resultsPerPage
+    for (let i = startIndex; i < startIndex + 20 && i < results.length; ++i) {
+      // console.log(i);
+      // console.log(results[i]);
+      page.push(results[i]);
+    }
+    // console.log(results);
+    console.log(page);
     console.log("found server: " + columns + " " + pageNum + " " + searchTerms);
-    // will use this dummy data to test the front end, until search fully works
-    let dummyData = {
-      col: columns,
-      pg: pageNum,
-      search: searchTerms,
-      msg: "search function will be implemented soon",
-    };
 
-    res.send(dummyData);
+    res.send(page);
     // res.send(dataToSend);
     // res.send("searching for columns " + columns + " on page " + pageNum + " for terms " + searchTerms);
   });
