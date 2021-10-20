fab71a754dd4497f7b895c986571c8f41c404187
Sentinent <Steaboo@gmail.com>
2021-10-20 11:19:53 -0700

    Backend improvements + Optimized search to stop early, smarter paging so we don't have to search through the entire dataset + Added support for search operators + Added "County County" and "Violation County" to update * Temporarily change the number of files parsed by the backend for 1 so dev doesn't take forever

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
-const {spawn} = require("child_process");
-const {spawnSync} = require("child_process");
+const { spawn } = require("child_process");
+const { spawnSync } = require("child_process");
 
 var app = express();
 const port = 5000;
@@ -28,27 +28,29 @@ fs.access("./parsed_data/data8.json", fs.constants.F_OK, (err) => {
     console.log("file does not exist");
 
     let pyprog = spawnSync("python3", ["./functions/main.py"]);
-    
+
     pyprog.on("close", (msg) => {
       console.log("closing py: ${code}");
     });
-    
+
     pyprog.stderr.on("data", (data) => {
       console.log("error: " + data);
     });
   }
 
   console.log("found data json files, loading into memory...");
-  
-  'use strict';
+
+  ("use strict");
   let num = 2;
   console.log("loading file 1");
-  JSONDATA = require('./parsed_data/data1.json');
+  JSONDATA = require("./parsed_data/data1.json");
+  /*
   while (num < 9){
       console.log("loading file " + num);
       JSONDATA = JSONDATA.concat(require('./parsed_data/data' + num +'.json'));
       num++;
   }
+  */
   console.log("Sucessfully loaded " + JSONDATA.length + " rows into memory");
   module.exports = JSONDATA;
 });
