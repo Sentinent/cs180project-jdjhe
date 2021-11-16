443a19f67c8068155d2c7a9d2d1c88e02f889d23
htan029 <htan029@ucr.edu>
2021-11-16 03:09:33 -0800

    only pie chart works, cannot fix other chart

--- frontend/src/pages/Custom.tsx
+++ frontend/src/pages/Custom.tsx
@@ -54,78 +54,77 @@ function Custom() {
         {
           type: chartOption,
           dataPoints: dataPoints,
+          toolTipContent: '<b>{label}</b>: {y}%',
+          indexLabelFontSize: 9,
           startAngle: 75,
         },
       ],
     };
     setConfig(configTemp);
-  }, [customOption, chartOption, dataPoints]);
-
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    const selection = {customOption};
-    console.log(selection);
-  }
+  }, [chartOption,dataPoints]);
 
   return (
     <>
       <section className="bg-light">
         <div className="container">
           <div className="row p-2">
-            <form onSubmit={handleSubmit}>
-              <label>Selection:</label>
-              <select value={customOption} onChange={(e) => setCustomOption(e.target.value)}>
-                <option value="Registration State">Registration State</option>
-                <option value="Issue Date">Issue Date</option>
-                <option value="Violation Time">Violation Time</option>
-                <option value="Violation Code">Violation Code</option>
-                <option value="Vehicle Make">Vehicle Make</option>
-                <option value="Vehicle Body Type">Vehicle Body Type</option>
-                <option value="Vehicle Year">Vehicle Year</option>
-                <option value="Street Name">Street Name</option>
-                <option value="Violation County">Violation County</option>
-              </select>
-              <select value={chartOption} onChange={(e) => setChartOption(e.target.value)}>
+            <div className="col m-3">
+            <select className="form-select form-select-md mb-3" value={customOption} onChange={(e) => setCustomOption(e.target.value)}>
+              <option value="Registration State">Registration State</option>
+              <option value="Issue Date">Issue Date</option>
+              <option value="Violation Time">Violation Time</option>
+              <option value="Violation Code">Violation Code</option>
+              <option value="Vehicle Make">Vehicle Make</option>
+              <option value="Vehicle Body Type">Vehicle Body Type</option>
+              <option value="Vehicle Year">Vehicle Year</option>
+              <option value="Street Name">Street Name</option>
+              <option value="Violation County">Violation County</option>
+            </select>
+            </div>
+            <div className="col m-3">
+              <select className="form-select form-select-md mb-3" value={chartOption} onChange={(e) => setChartOption(e.target.value)}>
                 <option value="pie">Pie</option>
                 <option value="doughnut">Doughnut</option>
-                <option value="bar">Bar</option>
+                {/* <option value="bar">Bar</option>
                 <option value="area">Area</option>
                 <option value="scatter">Scatter</option>
                 <option value="spline">Spline</option>
-                <option value="stepLine">Step Line</option>
+                <option value="stepLine">Step Line</option> */}
               </select>
-              <button className="btn">gen</button>
-            </form>
+            </div>
           </div>
         </div>
       </section>
       <section>
-        <div className="row text-center g-3 m-3">
-          <div className="col-xxl">
-            <CanvasJSChart options={config}></CanvasJSChart>
+        <div className="container">
+          <div className="row text-center g-3 m-3">
+            <div className="col-xxl">
+              <CanvasJSChart options={config}></CanvasJSChart>
+            </div>
           </div>
         </div>
       </section>
       <section>
-        <div className="row text-center g-3 m-3">
-          <div className="col-xxl">
-            <table className="table table-bordered">
-              <thead>
-                <tr>
-                  <th>{customOption}</th>
-                  <th>Percentage</th>
-                </tr>
-              </thead>
-              <tbody>
-                {dataPoints.map((_, index) => (
+        <div className="container">
+          <div className="row text-center g-3 m-3">
+            <div className="col-xxl">
+              <table className="table table-bordered">
+                <thead>
                   <tr>
-                    <td>{_.label}</td>
-                    <td>{_.y}%</td>
+                    <th>{customOption}</th>
+                    <th>Percentage</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {dataPoints.map((_, index) => (
+                    <tr>
+                      <td>{_.label}</td>
+                      <td>{_.y}%</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </section>
